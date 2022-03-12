4b303b274e3115cfb019b61f9fece0a5ed21ee47
CharlieLikesCake <47156862+CharlieLikesCake@users.noreply.github.com>
2022-03-11 23:54:12 -0500

    Update icon_buttons.dart

--- lib/widgets/icon_buttons.dart
+++ lib/widgets/icon_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:chatter/theme.dart';
 import 'package:flutter/material.dart';
 
 class IconBackground extends StatelessWidget {
@@ -13,17 +14,17 @@ class IconBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.transparent,
+      color: Theme.of(context).cardColor,
       borderRadius: BorderRadius.circular(6),
       child: InkWell(
         borderRadius: BorderRadius.circular(6),
+        splashColor: AppColors.secondary,
         onTap: onTap,
         child: Padding(
           padding: const EdgeInsets.all(6),
           child: Icon(
             icon,
             size: 22,
-            color: Colors.red,
           ),
         ),
       ),
@@ -45,6 +46,7 @@ class IconBorder extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       borderRadius: BorderRadius.circular(6),
+      splashColor: AppColors.secondary,
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
