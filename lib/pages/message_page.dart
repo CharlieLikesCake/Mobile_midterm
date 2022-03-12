3a753835fbe11998812368921b5a9fc7b63c72bf
CharlieLikesCake <47156862+CharlieLikesCake@users.noreply.github.com>
2022-03-11 23:12:51 -0500

    Update message_page.dart

--- lib/pages/message_page.dart
+++ lib/pages/message_page.dart
@@ -4,171 +4,271 @@ import 'package:chat_app/widgets/widgets.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:jiffy/jiffy.dart';
+import 'package:chatter/widgets/display_error_message.dart';
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
+import 'package:chatter/app.dart';
 
 import '../helper.dart';
 
-class MessagesPage extends StatelessWidget {
+class MessagesPage extends StatefulWidget {
   const MessagesPage({Key? key}) : super(key: key);
 
+  @override
+  _MessagesPageState createState() => _MessagesPageState();
+}
+
+class _MessagesPageState extends State<MessagesPage> {
+  final channelListController = ChannelListController();
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: _Stories()),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(_delegate),
+    return ChannelListCore(
+      channelListController: channelListController,
+      filter: Filter.and(
+        [
+          Filter.equal('type', 'messaging'),
+          Filter.in_('members', [
+            StreamChatCore.of(context).currentUser!.id,
+          ])
+        ],
+      ),
+      emptyBuilder: (context) => const Center(
+        child: Text(
+          'So empty.\nGo and message someone.',
+          textAlign: TextAlign.center,
         ),
-      ],
+      ),
+      errorBuilder: (context, error) => DisplayErrorMessage(
+        error: error,
+      ),
+      loadingBuilder: (
+        context,
+      ) =>
+          const Center(
+        child: SizedBox(
+          height: 100,
+          width: 100,
+          child: CircularProgressIndicator(),
+        ),
+      ),
+      listBuilder: (context, channels) {
+        return CustomScrollView(
+          slivers: [
+            const SliverToBoxAdapter(
+              child: _Stories(),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return _MessageTile(
+                    channel: channels[index],
+                  );
+                },
+                childCount: channels.length,
+              ),
+            )
+          ],
+        );
+      },
     );
-  }
-  Widget _delegate(BuildContext context, int index){
-    final Faker faker =Faker();
-    final date =Helpers.randomDate();
-    return _MessageTitle(
-      messageData: MessageData(
-        senderName: faker.person.name(),
-        message: faker.lorem.sentence(),
-        messageDate: date, 
-        dateMessage: Jiffy(date).fromNow(),
-        profilePicture: Helpers.randomPictureUrl(),
-        ),
-      );
   }
 }
 
-class _MessageTitle extends StatelessWidget {
-  const _MessageTitle({
-    Key? key, 
-    required this.messageData,
-    }) : super(key: key);
+class _MessageTile extends StatelessWidget {
+  const _MessageTile({
+    Key? key,
+    required this.channel,
+  }) : super(key: key);
 
- final MessageData messageData;
+  final Channel channel;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-          onTap:  (){
-            Navigator.of(context).push(ChatScreen.routeWithChannel(messageData));
-          },
-          child: Row(children: [
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Avatar.medium(url: messageData.profilePicture),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(messageData.senderName,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  letterSpacing: 0.2,
-                  wordSpacing: 1.5,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-              SizedBox(
-                height:20,
-                child: Text(
-                  messageData.message,
-                  overflow: TextOverflow.ellipsis,
-                  ),
-                ), 
-              ],
+      onTap: () {
+        Navigator.of(context).push(ChatScreen.routeWithChannel(channel));
+      },
+      child: Container(
+        height: 100,
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              width: 0.5,
             ),
           ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        messageData.dateMessage.toUpperCase(),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Avatar.medium(
+                    url:
+                        Helpers.getChannelImage(channel, context.currentUser!)),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        Helpers.getChannelName(channel, context.currentUser!),
+                        overflow: TextOverflow.ellipsis,
                         style: const TextStyle(
-                        fontSize: 11,
-                        letterSpacing: -0.2,
-                        fontWeight: FontWeight.w600,
+                          letterSpacing: 0.2,
+                          wordSpacing: 1.5,
+                          fontWeight: FontWeight.w900,
                         ),
                       ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Container(
-                        width: 18,
-                        height: 18,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Center(
-                          child: Text(
-                            '1',
-                            style: TextStyle(
-                              fontSize: 10,
-                            ),
-                          ),
-                        ),
-                    
-                      )
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      child: _buildLastMessage(),
+                    )
+                  ],
                 ),
-        ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    _buildLastMessageAt(),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Center(
+                      child: UnreadIndicator(
+                        channel: channel,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _buildLastMessage() {
+    return BetterStreamBuilder<int>(
+      stream: channel.state!.unreadCountStream,
+      initialData: channel.state?.unreadCount ?? 0,
+      builder: (context, count) {
+        return BetterStreamBuilder<Message>(
+          stream: channel.state!.lastMessageStream,
+          initialData: channel.state!.lastMessage,
+          builder: (context, lastMessage) {
+            return Text(
+              lastMessage.text ?? '',
+              overflow: TextOverflow.ellipsis,
+              style: (count > 0)
+                  ? const TextStyle(
+                      fontSize: 12,
+                    )
+                  : const TextStyle(
+                      fontSize: 12,
+                     ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildLastMessageAt() {
+    return BetterStreamBuilder<DateTime>(
+      stream: channel.lastMessageAtStream,
+      initialData: channel.lastMessageAt,
+      builder: (context, data) {
+        final lastMessageAt = data.toLocal();
+        String stringDate;
+        final now = DateTime.now();
+
+        final startOfDay = DateTime(now.year, now.month, now.day);
+
+        if (lastMessageAt.millisecondsSinceEpoch >=
+            startOfDay.millisecondsSinceEpoch) {
+          stringDate = Jiffy(lastMessageAt.toLocal()).jm;
+        } else if (lastMessageAt.millisecondsSinceEpoch >=
+            startOfDay
+                .subtract(const Duration(days: 1))
+                .millisecondsSinceEpoch) {
+          stringDate = 'YESTERDAY';
+        } else if (startOfDay.difference(lastMessageAt).inDays < 7) {
+          stringDate = Jiffy(lastMessageAt.toLocal()).EEEE;
+        } else {
+          stringDate = Jiffy(lastMessageAt.toLocal()).yMd;
+        }
+        return Text(
+          stringDate,
+          style: const TextStyle(
+            fontSize: 11,
+            letterSpacing: -0.2,
+            fontWeight: FontWeight.w600,
+          ),
+        );
+      },
     );
   }
 }
 
-
 class _Stories extends StatelessWidget {
   const _Stories({Key? key}) : super(key: key);
 
-   @override
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.only(top:8),
-      elevation: 0,
-      child: SizedBox(
-        height: 134,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: const Text(
-                'Stories',
-                style: TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 15,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Card(
+        elevation: 0,
+        child: SizedBox(
+          height: 140,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 16.0, top: 8, bottom: 16),
+                child: Text(
+                  'Stories',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 15,
+                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, int index) {
-                  final faker = Faker();
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      width: 60,
-                      child: _StoryCard(
-                        storyData: StoryData(
-                          name:faker.person.name(),
-                          url: Helpers.randomPictureUrl()
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    final faker = Faker();
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: 60,
+                        child: _StoryCard(
+                          storyData: StoryData(
+                            name: faker.person.firstName(),
+                            url: Helpers.randomPictureUrl(),
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
